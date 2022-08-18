6e1df852324d2153398f77c9e48fc5d63c514540
Yeswanth-Kaki <110684323+Yeswanth-Kaki@users.noreply.github.com>
2022-08-18 11:42:17 +0100

    Delete project directory

--- project/src/main/java/com/deloitte/project/repository/TodoTaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.deloitte.project.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.deloitte.project.model.TodoTask;
-
-public interface TodoTaskRepository extends CrudRepository<TodoTask,Integer>{
-
-}
